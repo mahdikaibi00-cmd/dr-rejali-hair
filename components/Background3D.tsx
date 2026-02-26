9083e841b3f9fed0e2eb33d71adfb3ce7f39f322
Mahdi Kaibi <mahdikaibi00@gmail.com>
2026-02-26 21:03:12 +0100

    fix: ignore maath typescript error for vercel build

--- components/Background3D.tsx
+++ components/Background3D.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
+// @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
 import { useState, useRef } from "react";
 
